c425e1707a75ed92dfed639183b9822316b1ae72
MayaS111 <mayasheriff2018@gmail.com>
2024-07-17 09:53:28 -0500

    comment removal

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,6 @@ class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
   before_action { authorize(@photo || Photo)}
-  # before_action :ensure_user_is_authorized, only: [:show]
 
 
   # GET /photos or /photos.json
@@ -71,11 +70,6 @@ class PhotosController < ApplicationController
         redirect_back(fallback_location: root_url, alert: "nice try, sucka")
       end
     end
-    # def ensure_user_is_authorized
-    #   if !PhotoPolicy.new(current_user, @photo).show?
-    #     raise Pundit::NotAuthorizedError, "not allowed"
-    #   end
-    # end
 
     # Only allow a list of trusted parameters through.
     def photo_params
